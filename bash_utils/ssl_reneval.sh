402c773ad2d7edbf827ea2f7160a59de42f7789c
Tomasz Sabała <sabala.tomek@gmail.com>
2019-04-24 15:30:20 +0200

    Added info logging time stamps for the script.

--- bash_utils/ssl_reneval.sh
+++ bash_utils/ssl_reneval.sh
@@ -7,7 +7,8 @@ set -x
 # USERNAME - name of the user in whom home dir meerkat_country_server is checked out
 # COUNTRY_NAME - name of deployed country e.g. car or demo
 #
-
+echo "$(date) ------- Running SSL reneval."
 ACTION=stop SERVICE=nginx country_server_console_wrapper
 certbot-auto renew
 ACTION=start SERVICE=nginx country_server_console_wrapper
+echo "$(date) ------- SSL reneval finished."
