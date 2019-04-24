0c5f7e32c8f745a1ce39941d31593b00ef75ba3c
Tomasz Sabała <sabala.tomek@gmail.com>
2019-04-24 11:20:45 +0200

    Adding helper bash scripts for basic docker-compose crontab automation.

---
+++ bash_utils/country_server_console_wrapper.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+### The following args should be passed as env variables
+# USERNAME - name of the user in whom home dir meerkat_country_server is checked out
+# COUNTRY_NAME - name of deployed country e.g. car or demo
+# ACTION - docker-compose action you'd like to use. e.g. 'up -d', 'stop', 'start', 'rm -v'
+# [optional] SERVICE - name of docker service e.g. odk, nest, nginx
+#
+# Example usage in crontab:
+# @reboot USERNAME=ec2-user COUNTRY_NAME=car ACTION='up -d' country_server_console_wrapper.sh  >> /var/log/country_startup.log 2>&1
+
+
+/usr/local/bin/docker-compose -f /home/${USERNAME}/meerkat_country_server/docker-compose.yml -f /home/${USERNAME}/meerkat_${COUNTRY_NAME}/nest/${COUNTRY_NAME} ${SERVICE} ${ACTION}
