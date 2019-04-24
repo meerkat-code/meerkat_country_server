73b9ac74371709e70173a5c3c108397520249e9d
Tomasz Sabała <sabala.tomek@gmail.com>
2019-04-24 10:20:18 +0200

    Cleaning up existing sym links in sties-enabled.

--- nginx/run.sh
+++ nginx/run.sh
@@ -5,6 +5,7 @@ envsubst < /etc/nginx/sites-available/odk_aggregate_https.template | tee /etc/ng
 
 echo "---- Creating symlinks ----"
 mkdir -p /etc/nginx/sites-enabled
+rm -f /etc/nginx/sites-enabled/odk_aggregate_http*
 ln -s /etc/nginx/sites-available/odk_aggregate_http /etc/nginx/sites-enabled/odk_aggregate_http
 ln -s /etc/nginx/sites-available/odk_aggregate_https /etc/nginx/sites-enabled/odk_aggregate_https
 
